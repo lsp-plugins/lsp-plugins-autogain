5179fc3489afc9a997ac5b157d352166b6e9ce2c
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-09-21 23:31:47 +0300

    Initial import from plugin template

---
+++ include/private/plugins/autogain.h
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2023 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2023 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-autogain
+ * Created on: 21 сен 2023 г.
+ *
+ * lsp-plugins-autogain is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-autogain is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-autogain. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PRIVATE_PLUGINS_AUTOGAIN_H_
+#define PRIVATE_PLUGINS_AUTOGAIN_H_
+
+#include <lsp-plug.in/dsp-units/util/Delay.h>
+#include <lsp-plug.in/dsp-units/ctl/Bypass.h>
+#include <lsp-plug.in/plug-fw/plug.h>
+#include <private/meta/autogain.h>
+
+namespace lsp
+{
+    namespace plugins
+    {
+        /**
+         * Base class for the latency compensation delay
+         */
+        class autogain: public plug::Module
+        {
+            private:
+                autogain & operator = (const autogain &);
+                autogain (const autogain &);
+
+            protected:
+                enum mode_t
+                {
+                    CD_MONO,
+                    CD_STEREO,
+                    CD_X2_STEREO
+                };
+
+                typedef struct channel_t
+                {
+                    // DSP processing modules
+                    dspu::Delay         sLine;              // Delay line
+                    dspu::Bypass        sBypass;            // Bypass
+
+                    // Parameters
+                    ssize_t             nDelay;             // Actual delay of the signal
+                    float               fDryGain;           // Dry gain (unprocessed signal)
+                    float               fWetGain;           // Wet gain (processed signal)
+
+                    // Input ports
+                    plug::IPort        *pIn;                // Input port
+                    plug::IPort        *pOut;               // Output port
+                    plug::IPort        *pDelay;             // Delay (in samples)
+                    plug::IPort        *pDry;               // Dry control
+                    plug::IPort        *pWet;               // Wet control
+
+                    // Output ports
+                    plug::IPort        *pOutDelay;          // Output delay time
+                    plug::IPort        *pInLevel;           // Input signal level
+                    plug::IPort        *pOutLevel;          // Output signal level
+                } channel_t;
+
+            protected:
+                size_t              nChannels;          // Number of channels
+                channel_t          *vChannels;          // Delay channels
+                float              *vBuffer;            // Temporary buffer for audio processing
+
+                plug::IPort        *pBypass;            // Bypass
+                plug::IPort        *pGainOut;           // Output gain
+
+                uint8_t            *pData;              // Allocated data
+
+            protected:
+                void                do_destroy();
+
+            public:
+                explicit autogain(const meta::plugin_t *meta);
+                virtual ~autogain() override;
+
+                virtual void        init(plug::IWrapper *wrapper, plug::IPort **ports) override;
+                virtual void        destroy() override;
+
+            public:
+                virtual void        update_sample_rate(long sr) override;
+                virtual void        update_settings() override;
+                virtual void        process(size_t samples) override;
+                virtual void        dump(dspu::IStateDumper *v) const override;
+        };
+
+    } /* namespace plugins */
+} /* namespace lsp */
+
+
+#endif /* PRIVATE_PLUGINS_AUTOGAIN_H_ */
+
---
+++ src/main/ui/autogain.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2023 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2023 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-autogain
+ * Created on: 21 сен 2023 г.
+ *
+ * lsp-plugins-autogain is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-autogain is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-autogain. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <lsp-plug.in/plug-fw/ui.h>
+#include <private/plugins/autogain.h>
+
+namespace lsp
+{
+    namespace plugui
+    {
+        //---------------------------------------------------------------------
+        // Plugin UI factory
+        static const meta::plugin_t *plugin_uis[] =
+        {
+            &meta::autogain_mono,
+            &meta::autogain_stereo
+        };
+
+        static ui::Factory factory(plugin_uis, 2);
+
+    } /* namespace plugui */
+} /* namespace lsp */
+
+
